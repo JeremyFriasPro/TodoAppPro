c158b41142bea5c74cb1485a3754f8eb7add46f8
Jeremy B. Frias <jeremyfrias.pro@gmail.com>
2023-02-27 18:02:29 -0500

    All features for Version 1.0

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const form = document.querySelector('#todo-form');
 const todo = document.querySelector('#todo');
 const createButton = document.querySelector('#create-btn');
 const view = document.querySelector('.user-todos');
+const deleteAll = document.querySelector('#delete');
 
 
 const createTodo = (text) => {
@@ -11,14 +12,40 @@ const createTodo = (text) => {
 
     const cancelSlot = document.createElement('div');
     cancelSlot.className = 'cancel';
+    
+    const xImage = document.createElement('img');
+    xImage.src = '/cancel.png';
+    xImage.style.width = '40px';
+    xImage.style.height = '40px';
+
+    xImage.addEventListener('mouseover', () => {
+        
+    })
+
+    cancelSlot.addEventListener('click', () => {
+        deleteTodo(slot);
+    })
+
+    cancelSlot.addEventListener('mouseover', () => {
+        xImage.classList.toggle('lge');
+    })
 
     slot.append(cancelSlot);
     view.append(slot);
+    cancelSlot.append(xImage);
+
+    deleteAll.addEventListener('click', () => {
+        for(item of view.children) {
+            if(item.classList.contains('items')) {
+                deleteTodo(item);
+            }
+        }
+    })
 
 }
 
-const deleteTodo = () => {
-
+const deleteTodo = (todo) => {
+    todo.remove();
 }
 
 form.addEventListener('submit', (e) => {
